f19f3b98e90f8c4fba10a432e82f0ea30d9d336b
Ulysse Valdenaire <uvaldenaire@gmail.com>
2020-12-10 18:43:36 +0100

    ajouts

--- operationGame.py
+++ operationGame.py
@@ -1,50 +1,81 @@
 import random
 
-#INTRODUCTION
-print("Bonjour, tu vas t'entrainer aux mutiplications:")
-
-#GENERER LES DEUX CHIFFRES AU HASARD ENTRE 1 ET 10
-
-
-
-#ESSAI
-score = 0
-
-while score < 3:
-
-    a = random.randint(1, 10)
-    b = random.randint(1, 10)
-
-    c = a * b
-    print(a, "x", b, " = ?")
-    rep = input()
-
-
-    if int(rep) == c:
-        print("OK")
-        score = score + 1
-    else:
-        print("PAS OK")
-        score = score + 1
-
-    print("Bravo !!")
-
+#MULTIPLICATIONS
+def mul():
+    print("Tu vas t'entraîner aux multiplications ! Ton but est d'atteindre le score de 10 !")
     score = 0
+    while score < 10:
+        a = random.randint(1, 10)
+        b = random.randint(1, 10)
+        c = a * b
+        print(a, "x", b, " = ?")
+        rep = input()
+        if int(rep) == c:
+            print("OK, tu as juste.")
+            score = score + 1
+            print(f"Ton score est de {score}")
+        else:
+            print("PAS OK")
+            score = score - 1
+            print(f"Ton score est de {score}")
 
-while score < 3:
+    print("Bravo, tu as atteint 10 points !")
 
-    a = random.randint(1, 10)
-    b = random.randint(1, 10)
+#ADDITIONS
+def add():
+    print("Tu vas t'entraîner aux additions ! Ton but est d'atteindre le score de 10 !")
+    score = 0
+    while score < 10:
+        a = random.randint(1, 10)
+        b = random.randint(1, 10)
+        c = a+b
+        print(a, "+", b, " = ?")
+        rep = input()
 
-    c = a * b
-    print(a, "x", b, " = ?")
-    rep = input()
+        if int(rep) == c:
+            print("OK? Tu as juste !")
+            score = score + 1
+            print(f"ton score est{score}")
+        else:
+            print("PAS OK, c'est faux")
+            score = score + 1
+            print(f"ton score est{score}")
 
-    if int(rep) == c:
-        print("OK")
-        score = score + 1
-    else:
-        print("PAS OK")
-        score = score + 1
+    print("Bravo !! Tu as atteint 10 points !")
 
-    print("Bravo !!")
+#SOUSTRACTIONS
+def sous():
+    score = 0
+    print("Tu vas t'entraîner aux soustractions ! Ton but est d'atteindre le score de 10 !")
+
+    while score < 10:
+        a = random.randint(1, 10)
+        b = random.randint(1, 10)
+        c = a-b
+        print(a, "-", b, " = ?")
+        rep = input()
+
+        if int(rep) == c:
+            print("OK, tu as juste !")
+            score = score + 1
+            print(f"ton score est{score}")
+        else:
+            print("PAS OK")
+            score = score + 1
+            print(f"ton score est{score}")
+
+    print("Bravo !! Tu as atteint 10 points")
+
+
+
+#MENU PRINCIPAL
+level = input("Dans ce jeu, tu peux t'entrainer aux multiplications,\n"
+                "aux additions ou aux soustractions. Choisis en entrant une des trois lettre:\n"
+              "Multiplivations (M), additions (A), soustractions (S) : ")
+
+if level == "M":
+    mul()
+elif level == "A":
+    add()
+else:
+    sous()
